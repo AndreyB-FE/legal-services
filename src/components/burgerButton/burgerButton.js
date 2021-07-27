a5420708235635afb6f9cf39522a554e506d86fa
AndreyB-FE <andrei.228.bogushevich@gmail.com>
2021-07-27 16:47:19 +0300

    feat 'close mobile menu by clicking outside'

--- src/components/burgerButton/burgerButton.js
+++ src/components/burgerButton/burgerButton.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./burgerButton.scss";
 
 const BurgerButton = (props) => {
+  useEffect(() => {
+    props.headerRef.current.scrollIntoView();
+    if (props.isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  });
+
   return (
     <div
       id="burgerButton"
       className={`menu${props.isOpen ? " cross" : ""}`}
-      onClick={props.clickHandler}
+      onClick={() => props.clickHandler()}
     >
       <span></span>
       <span></span>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./header.scss";
 import BurgerButton from "../burgerButton/burgerButton";
 import HeaderLinks from "../headerLinks/headerLinks";
@@ -7,17 +7,22 @@ import Logo from "../logo/logo";
 import MobileMenu from "../mobileHeaderMenu/mobileHeaderMenu";
 
 const Header = () => {
+  const myRef = useRef(null);
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <header>
+    <header ref={myRef}>
       <Logo width={"50px"}></Logo>
       <HeaderLinks></HeaderLinks>
       <OrderButton></OrderButton>
       <BurgerButton
         isOpen={isOpen}
+        headerRef={myRef}
         clickHandler={() => setIsOpen(!isOpen)}
       ></BurgerButton>
-      <MobileMenu isOpen={isOpen}></MobileMenu>
+      <MobileMenu
+        isOpen={isOpen}
+        clickHandler={() => setIsOpen(!isOpen)}
+      ></MobileMenu>
     </header>
   );
 };
